2bc513fe5b94715b515f03e02078c8428df78444
sonsuchang <55570227+sonsuchang@users.noreply.github.com>
2025-03-05 17:25:41 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,42 @@
+-- 코드를 작성해주세요
+with cte as (
+    select * from developers d
+        left join skillcodes s on d.skill_code & s.code
+),
+
+A_sub as (
+    select id from cte
+    where name = "Python"
+),
+
+A as (
+    select "A" as Grade, id, email from cte
+    where id in (
+        select * from A_sub
+    ) and category = "Front End"
+),
+
+B as (
+    select "B" as Grade, id, email from cte
+    where name = "C#"
+),
+
+C as (
+    select "C" as Grade, id, email from cte
+    where category = "Front End" and id not in (
+        select id from A
+        union
+        select id from B
+    )
+),
+
+result as (
+    select * from A
+    union
+    select * from B
+    union
+    select * from C
+)
+
+select * from result
+order by Grade, id
