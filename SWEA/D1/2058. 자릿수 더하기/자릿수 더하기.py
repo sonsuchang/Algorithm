69ba31da4df63f6c4de5b7f3ce6d21ad27d674fe
sonsuchang <55570227+sonsuchang@users.noreply.github.com>
2025-07-07 17:28:42 +0900

    [D1] Title: 자릿수 더하기, Time: 63 ms, Memory: 49,664 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,16 @@
-T = list(input().rstrip())
-result = 0
-for i in T:
-    result += int(i)
-print(result)
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int length = s.length();
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+        	sum += Character.getNumericValue(s.charAt(i));
+        }
+		System.out.println(sum);
+	}
+}
