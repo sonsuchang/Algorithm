8e83103cfbf03893af34dc47a69c1d869b3facde
sonsuchang <55570227+sonsuchang@users.noreply.github.com>
2025-09-01 12:21:32 +0900

    [Silver II] Title: 색종이 만들기, Time: 132 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,49 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int N, wCnt, bCnt;
-    static int[][] map;
+	static int N;
+	static int wCnt = 0;
+	static int bCnt = 0;
+	static int[][] map;
+	
+	public static void main(String[] args) throws IOException {
+		//--------------솔루션 코드를 작성하세요.---------------------------
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        wCnt = 0;
-        bCnt = 0;
-        map = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(s[j]);
-            }
-        }
-        divide(0, 0, N);
-        System.out.println(wCnt);
-        System.out.println(bCnt);
-    }
-
-    public static void divide(int r, int c, int size) {
-
-        for (int i = r; i < r + size; i++) {
-            for (int j = c; j < c + size; j++) {
-                int color = map[r][c];
-                if (map[i][j] != color) {
-                    int next = size / 2;
-                    divide(r, c, next);
-                    divide(r, c + next, next);
-                    divide(r + next, c, next);
-                    divide(r + next, c + next, next);
-                    return;
-                }
-            }
-        }
-
-        if (map[r][c] == 0) wCnt++;
-        else bCnt++;
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			String[] s = br.readLine().split(" ");
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(s[j]);
+			}
+		}
+		
+		
+		divide(0, 0, N);
+		
+		System.out.println(wCnt);
+		System.out.println(bCnt);
+	}
+	
+	public static void divide(int r, int c, int depth) {
+		for (int i = r; i < r + depth; i++) {
+			for (int j = c; j < c + depth; j++) {
+				if (map[i][j] != map[r][c]) {
+					int next = depth / 2;
+					divide(r, c, next);
+					divide(r + next, c, next);
+					divide(r, c + next, next);
+					divide(r + next, c + next, next);
+					return;
+				}
+			}
+		}
+		
+		if (map[r][c] == 0) wCnt++;
+		else bCnt++;
+	}
 }
